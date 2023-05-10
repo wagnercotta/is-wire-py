0b0b11cf9677cb0b83e87a08d29532caaf6b95eb
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2023-05-10 07:41:47 -0300

    feat: github actions for ci pipeline (#3)
    * chore: add pull request template
    
    * chore: add issues templates
    
    * chore: remove tox.ini file
    
    * test: remove travis ci
    
    * tests: add workflow to test packge
    
    * tests: removed appveyoy files
    
    * fix: include flake8 in envlist
    
    * fix: rename workflow
    
    * chore: bump dependencies (amqp=5.1.1 and protobuf=3.20.3)

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='is_wire',
     version='1.2.0',
-    description='',
+    description='Python implementation of application support layer.',
+    long_description=open("README.md").read().strip(),
+    long_description_content_type="text/markdown",
     url='http://github.com/labviros/is-wire-py',
     author='labviros',
     license='MIT',
@@ -12,10 +14,9 @@ setup(
     zip_safe=False,
     install_requires=[
         'colorlog==3.1.4',
-        'amqp==2.4.2',
-        'enum34==1.1.6',
-        'protobuf==3.6.0',
+        'amqp==5.1.1',
+        'protobuf==3.20.3',
         'opencensus==0.5.0',
         'prometheus-client==0.3.1',
-    ]
+    ],
 )
