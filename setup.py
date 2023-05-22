67fcd9963224799e2e1a7fdb4734b62e75c7c470
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2023-05-22 12:55:05 -0300

    fix: typo in classifiers in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='http://github.com/labvisio/is-wire-py',
     classifiers=[
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
