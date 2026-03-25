d8a9804b6e7910eeaa0c8fbab9b786c0987b1219
Wagner <waacotta@gmail.com>
2026-03-25 19:42:42 -0300

    refactor: remove tracing support

--- src/is_wire/core/__init__.py
+++ src/is_wire/core/__init__.py
@@ -1,12 +1,10 @@
 from is_wire.core.channel import Channel
-from is_wire.core.subscription import Subscription
-from is_wire.core.message import Message
 from is_wire.core.logger import Logger
-from is_wire.core.utils import now, new_uuid
-from is_wire.core.wire.status import Status, StatusCode
+from is_wire.core.message import Message
+from is_wire.core.subscription import Subscription
+from is_wire.core.utils import new_uuid, now
 from is_wire.core.wire.content_type import ContentType
-from is_wire.core.tracing.tracer import Tracer
-from opencensus.common.transports.async_ import AsyncTransport
+from is_wire.core.wire.status import Status, StatusCode
 
 __all__ = [
     "Channel",
@@ -18,6 +16,4 @@ __all__ = [
     "Status",
     "StatusCode",
     "ContentType",
-    "Tracer",
-    "AsyncTransport",
 ]
--- src/is_wire/rpc/__init__.py
+++ src/is_wire/rpc/__init__.py
@@ -1,13 +1,11 @@
-from is_wire.rpc.service_provider import ServiceProvider
 from is_wire.rpc.interceptor import Interceptor
 from is_wire.rpc.log_interceptor import LogInterceptor
-from is_wire.rpc.tracing_interceptor import TracingInterceptor
 from is_wire.rpc.metrics_interceptor import MetricsInterceptor
+from is_wire.rpc.service_provider import ServiceProvider
 
 __all__ = [
     "ServiceProvider",
     "Interceptor",
     "LogInterceptor",
-    "TracingInterceptor",
     "MetricsInterceptor",
 ]
